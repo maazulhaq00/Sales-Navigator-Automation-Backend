e8371bb0dbc7cf9eee611f2232402dd6292caecf
maazulhaq00 <maazulhaq00@gmail.com>
2022-07-24 18:10:22 +0500

    campaigns apis

--- middleware/auth.js
+++ middleware/auth.js
@@ -10,14 +10,14 @@ const auth = async (req, res, next) => {
       "tokens.token": token,
     });
     if (!user) {
-      throw new Error();
+      throw new Error("Faild to authenticate");
     }
 
     req.token = token;
     req.user = user;
     next();
   } catch (e) {
-    res.status(401).send({ message: "Please authenticate." });
+    res.status(401).send({ message: "Faild to authenticate", errMessage: e.message });
   }
 };
 
--- models/userCampaign.js
+++ models/userCampaign.js
@@ -4,82 +4,125 @@ const Leads = require("./lead");
 const { Schema, Types } = mongoose;
 
 const schema = new mongoose.Schema({
-  campaignTitle: {
+  userId: {
+    type: Types.ObjectId,
+    required: true
+  },
+  isAdmin: {
+    type: Boolean,
+    required: true
+  },
+  title: {
     type: String,
     required: true,
     trim: true,
     validate(value) {
       if (validator.isEmpty(value)) {
-        throw new Error("campaign title cannot be");
+        throw new Error("campaign title cannot be empty");
       }
     },
   },
+  description: {
+    type: String,
+    trim: true,
+  },
+  connectionRequest: {
+    type: String,
+    trim: true,
+  },
+  followups: [
+    {
+      days: {
+        type: Number,
+        trim: true,
+        required: true,
+        validate(value) {
+          if (value > 365) {
+            throw new Error("Follow-up days should not be more than 365 days");
+          }
+        },
+      },
+      hours: {
+        type: Number,
+        trim: true,
+        required: true,
+        validate(value) {
+          if (value > 24) {
+            throw new Error("Follow-up hours should not be more than 24 hours");
+          }
+        },
+      },
+      followUpMessage: {
+        type: String,
+        trim: true,
+        required: true,
+      },
+    },
+  ],
   typingDelay: {
     type: Number,
     required: true,
     trim: true,
     validate(value) {
       if (value > 60000) {
-        throw new Error("Typing delay is should be less than 60000ms / 60s");
+        throw new Error("Typing delay should not exceed 60000ms / 60s");
       }
     },
   },
-  delayPageNavigate: {
+  pageNavigationDelay: {
     type: Number,
     required: true,
     trim: true,
     validate(value) {
       if (value > 600000) {
-        throw new Error("Typing delay is should be less than 600000ms / 10m");
+        throw new Error("Typing delay should not exceed 600000ms / 10m");
       }
     },
   },
-  noOfProfileVisitsPerHour: {
+  noOfVisitsPerHr: {
     type: Number,
     required: true,
     trim: true,
     validate(value) {
       if (value > 100) {
         throw new Error(
-          "No of profile visits per hour should be less than 100"
+          "No of profile visits per hour should not exceed 100 visits"
         );
       }
     },
   },
-
-  noOfProfileVisitsPerDay: {
+  noOfVisitsPerDay: {
     type: Number,
     required: true,
     trim: true,
     validate(value) {
       if (value > 1300) {
         throw new Error(
-          "No of profile visits per hour should be less than 1300"
+          "No of profile visits per day should not exceed 1300 visits"
         );
       }
     },
   },
-
-  visitsBeforeDelay: {
+  profileVisitsBeforeDelay: {
     type: Number,
     required: true,
     trim: true,
     validate(value) {
       if (value > 1000) {
         throw new Error(
-          "No of profile visits before delay should be less than 1000"
+          "No of profile visits before delay should not exceed 1000 visits"
         );
       }
     },
   },
-  visitDelay: {
+  profileVisitDelay: {
     type: Number,
     required: true,
     trim: true,
     validate(value) {
       if (value > 3600000) {
         throw new Error(
-          "delay profile visits per hour should be less than 3600000ms / 1h"
+          "The delay after certain no of profile visits should not exceed 3600000ms / 1h"
         );
       }
     },
@@ -91,12 +134,12 @@ const schema = new mongoose.Schema({
     validate(value) {
       if (value > 365) {
         throw new Error(
-          "limit snooze days after reaching the limit is less than 365 days"
+          "Snooze days should not exceed 365 days"
         );
       }
     },
   },
-  workingDaysArr: {
+  workingDaysArr: [{
     type: String,
     required: true,
     trim: true,
@@ -105,20 +148,16 @@ const schema = new mongoose.Schema({
         throw new Error("working days cannot be empty");
       }
     },
-  },
+  }],
   workHrsPerDay: {
     type: Number,
     required: true,
     trim: true,
     validate(value) {
       if (value > 24) {
-        throw new Error("work hrs per day should be less than 24");
+        throw new Error("work hrs per day should be less than 24 hrs");
       }
     },
-  },
-  userCampaignId: {
-    type: Types.ObjectId,
-    ref: "UserCampaign",
   },
 });
 
